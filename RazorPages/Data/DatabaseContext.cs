81abee2f5d03d6be6f61b786c507e6760ea3ef83
Paulina.Bury <javadevil321@gmail.com>
2023-03-01 19:34:44 +0100

    Initialize

---
+++ RazorPages/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        :base(options)
+        { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
